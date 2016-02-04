df1b81584c88044829de5f98bb568dbbdbe3b6d7
Francesco Cavazzana <francesco@cavazzana.it>
2016-02-05 00:00:40 +0100

    remote command (not yet working)

--- autopilot1/autopilot1/autopilot1.ino
+++ autopilot1/autopilot1/autopilot1.ino
@@ -2,6 +2,8 @@
 #include <NewPing.h>
 #include <SoftwareSerial.h>
 
+#define DEBUG_NO_MOTOR false
+
 #define A_motor_pwm 3
 #define A_motor_brake 9
 #define A_motor_dir 12
@@ -15,7 +17,7 @@
 #define Steering_motor 5
 
 #define Dist_echo 2
-#define Dist_trig 4
+#define Dist_trig 2
 #define max_distance_cm 200
 
 #define Pot_input A2
@@ -27,11 +29,21 @@
 #define MIN_SPEED 30 // minimum speed for motor to move
 #define TIME_TO_BRAKE 2000 // time to stop from top speed
 
+#define CURRENT_TO_MA 2.96 // 2A at 3.3 volt: convert analog read to mA 
+#define MAX_TORQUE_MA 500 // lego medium motor max torque 300
+
 #define MAX_STEER 55 // maximum steering angle for servo
 #define STEER_TRIM 25// adjust 0 angle
 
 #define BT_RX A5 // bluetooth
 #define BT_TX A4
+#define TIME_UNIQUE_MANEUVER 5000 //if less than 5 seconds from last maneuver, it is an unique maneuver
+
+#define BT_COMM_DELAY 50 // bluetooth communication frequency
+
+#define DIST_HALF_STEER 100
+#define DIST_FULL_STEER 100
+#define DIST_BRAKE 100
 
 int last_brake = 0;
 int last_curr = 0;
@@ -51,22 +63,33 @@ int cur_steering = 0;
 long last_dist;
 long last_dist_check = millis();
 
-int consumo;
+long last_brake_and_back_time;
+int brake_and_back_direction;
+
+int current[10];
+
 
 bool strategyLock = false;
 char strategy = 'Z'; // current strategy
-char strategyState = 'Z'; // current strategy state
+char strategyStep = 'Z'; // current strategy state
 unsigned long strategyStart; // time since strategy start
-unsigned long strategyStateStart; // time since strategy state start
+unsigned long strategyStepStart; // time since strategy state start
+
+unsigned long lastCommCicles = 0; // cicles since last communication
+unsigned long lastCommTime = 0;
 
 SoftwareSerial bluetooth(BT_RX, BT_TX);
 
 Servo steering_servo;
 NewPing sonar(Dist_trig, Dist_echo, max_distance_cm);
 
+String stringOne;
+String debugLog;
+String btLog;
+
 void setup() {
   Serial.begin( 250000 );
-  bluetooth.begin(9600); //set baud rate
+  bluetooth.begin( 115200); //set baud rate
 
   // put your setup code here, to run once:
   pinMode(A_motor_pwm, OUTPUT);
@@ -88,55 +111,52 @@ void setup() {
   steering_servo.attach(Steering_motor);
 
   digitalWrite(A_motor_dir, dir_A);
-
+  lastCommCicles = 0;
+  lastCommTime = 0;
   forward();
   stopEngine();
 }
 
-void loop_test() {
-  int steer = analogRead(Pot_input);
-  if (steer != cur_steering) {
-    int val = map(steer, 0, 1023, 0, 180);
-    steering_servo.write(val);
-
-    cur_steering = steer;
-    bluetooth.println(val);
-  }
-
-
-}
-
 void loop() {
+  debugLog = String();
+  lastCommCicles += 1;
   long dist;
+  unsigned long ms = millis();
+  unsigned long ms_tot = ms;
   if ((millis() - last_dist_check) > 100) {
     dist = checkDistanceNew();
-    if (dist==0){
-      dist = checkDistanceNew();
+
+    for (int i=9; i > 0; i -= 1) {
+      current[i] = current[i-1];
     }
-    consumo = analogRead(A0);
+    current[0] = analogRead(A0) * CURRENT_TO_MA;
     last_dist_check = millis();
   } else  {
     dist = last_dist;
   }
-
+  debugLog = debugLog + "dist_check: " + (millis() - ms) + "\n";
+  
   int delta_dist = dist - last_dist;
   last_dist = dist;
 
+  ms = millis();
   checkManualStrategy();
+  debugLog = debugLog + "checkManualStrategy: " + (millis() - ms) + "\n";
 
+  ms = millis();
   if (strategyLock == false) {
-    if (consumo > 100 ) {
-      bluetooth.println("consumo elevato");
+    if (current[0] > MAX_TORQUE_MA) {
+      btLog = btLog + "# high current\n";
       brakeAndBack();
-    } else if (dist >= 100) { // current direction is good
+    } else if (dist >= DIST_HALF_STEER) { // direction is good
       goAhead();
     }
-    else if (dist < 20) { // emergency brake
+    else if (dist < DIST_BRAKE) { // emergency brake
       brakeAndBack();
-    } else if (dist < 50) { // full steer
+    } else if (dist < DIST_FULL_STEER) { // full steer
       steerFull();
     }
-    else if (dist < 100) { // half steer
+    else if (dist < DIST_HALF_STEER) { // half steer
       steerHalf();
     }
 
@@ -163,10 +183,55 @@ void loop() {
       case 'Y':
         manualSpeed();
         break;
+      case 'M':
+        // manual command from bluetooth: do nothing, commands are given in checkManualStrategy
+        break;
     }
   }
-  motorLoop();
+  debugLog = debugLog + "checkAutoStrategy: " + (millis() - ms) + "\n";
 
+  ms = millis();
+  motorLoop();
+  debugLog = debugLog + "motorLoop: " + (millis() - ms) + "\n";
+
+  ms = millis();
+  communicate();
+  debugLog = debugLog + "communicate: " + (millis() - ms) + "\n";
+
+  debugLog = debugLog + "total: " + (millis() - ms_tot) + "\n";
+
+  ms = millis();
+  Serial.println(debugLog);
+  Serial.println(String("spedizione stringa: ") + (millis() - ms));
+}
+
+
+void communicate() {
+  unsigned long timer = millis();
+  if (btLog.length() == 0 && (timer - lastCommTime) < BT_COMM_DELAY){
+    return;
+  }
+  if (btLog.length() > 0){
+      bluetooth.println(btLog);
+      btLog = String();
+  }
+  stringOne = String(timer) + ";" + last_dist + ";" + cur_steering + ";" + dir_A + ";"
+              + brake_A + ";" + speed_A + ";" + target_speed_A + ";" + current[0] + ";"
+              + strategyLock + ";" + strategy + ";" + strategyStart + ";"
+              + strategyStep + ";" + strategyStepStart + ";" + lastCommCicles + ";" + lastCommTime;
+  bluetooth.println(stringOne);
+  //Serial.print("spedisco BT: ");
+  //Serial.println(millis() - timer);
+
+  //timer = millis();
+  stringOne = String();
+  while (bluetooth.available()) {
+    stringOne += char(bluetooth.read());
+  }
+  //Serial.print("leggo BT: ");
+  //Serial.println(millis() - timer);
+  lastCommTime = millis();
+  lastCommCicles = 0;
 }
 
 void manualSteer() {
@@ -189,42 +254,72 @@ void manualSpeed() {
   }
 }
 
-
+void serialRemote() {
+  bool start = startStrategy('M');
+  strategyLock = true;
+  int dir = int(stringOne[1]);
+  if (dir == 0) {
+    reverse();
+  } else {
+    forward();
+  }
+  int speed_ = stringOne.substring(2, 4).toInt();
+  int steer_ = stringOne.substring(4, 6).toInt();
+  int brake = int(stringOne[7]);
+  if(brake==1){
+    accelerate(-1);
+  } else {
+    accelerate(speed_);
+  }
+  steer(steer_);
+}
 
 void checkManualStrategy() {
-  if (digitalRead(Btn_1) == HIGH && last_btn_1 == LOW && digitalRead(Btn_2) == HIGH && last_btn_2 == LOW) {
-    if (strategy == 'X') {
-      strategyLock = false;
-    } else {
-      stopEngine();
+  if (stringOne.length() > 0) { //
+    switch (stringOne[0]) {
+      case 'A':
+        if(strategy=='M'){
+          strategyLock = false;
+        }
+      case 'M':
+        serialRemote();
     }
+    stringOne = String();
   }
 
-  if (digitalRead(Btn_1) == HIGH && last_btn_1 == LOW) {
-    last_btn_1 == HIGH;
-    if (strategy == 'X') {
-      strategyLock = false;
-    } else {
-      manualSteer();
-    }
-  }
-  if (digitalRead(Btn_1) == LOW) {
-    last_btn_1 == LOW;
-    delay(100);
-  }
-
-  if (digitalRead(Btn_2) == HIGH && last_btn_2 == LOW) {
-    last_btn_2 == HIGH;
-    if (strategy == 'Y') {
-      strategyLock = false;
-    } else {
-      manualSpeed();
-    }
-  }
-  if (digitalRead(Btn_1) == LOW) {
-    last_btn_1 == LOW;
-    delay(100);
-  }
+//  if (digitalRead(Btn_1) == HIGH && last_btn_1 == LOW && digitalRead(Btn_2) == HIGH && last_btn_2 == LOW) {
+//    if (strategy == 'X') {
+//      strategyLock = false;
+//    } else {
+//      stopEngine();
+//    }
+//  }
+//
+//  if (digitalRead(Btn_1) == HIGH && last_btn_1 == LOW) {
+//    last_btn_1 == HIGH;
+//    if (strategy == 'X') {
+//      strategyLock = false;
+//    } else {
+//      manualSteer();
+//    }
+//  }
+//  if (digitalRead(Btn_1) == LOW) {
+//    last_btn_1 == LOW;
+//    delay(100);
+//  }
+//
+//  if (digitalRead(Btn_2) == HIGH && last_btn_2 == LOW) {
+//    last_btn_2 == HIGH;
+//    if (strategy == 'Y') {
+//      strategyLock = false;
+//    } else {
+//      manualSpeed();
+//    }
+//  }
+//  if (digitalRead(Btn_1) == LOW) {
+//    last_btn_1 == LOW;
+//    delay(100);
+//  }
 
 
 }
@@ -235,25 +330,21 @@ bool startStrategy(char st) {
   } else {
     strategy = st;
     strategyStart = millis();
-    strategyStateStart = millis();
-    strategyState = 'Z';
-    bluetooth.print("New strategy: ");
-    bluetooth.print(st);
-    bluetooth.print(" - dist: ");
-    bluetooth.println(last_dist);
+    strategyStepStart = millis();
+    strategyStep = 'Z';
+    btLog = btLog + "# New strategy: " + st + " - dist: " + last_dist + "\n";
     return true;
   }
 
 }
 
-bool startStrategyState(char st) {
-  if (st == strategyState) {
+bool startstrategyStep(char st) {
+  if (st == strategyStep) {
     return false; // strategy is in esecution
   } else {
-    strategyState = st;
-    strategyStateStart = millis();
-    bluetooth.print("New strategy state: ");
-    bluetooth.println(st);
+    strategyStep = st;
+    strategyStepStart = millis();
+    btLog = btLog + "# New strategy state: " + st + "\n";
     return true;
   }
 }
@@ -278,53 +369,61 @@ void brakeAndBack() {
   bool start = startStrategy('B');
   if (start) {
     strategyLock = true;
-    bluetooth.println("strategyLock = true");
+    btLog = btLog + "# strategyLock = true\n";
     accelerate(-1);
     steer(0);
-    startStrategyState('A');
+    startstrategyStep('A');
     return;
   }
-  //bluetooth.println(strategyState);
+  //bluetooth.println(strategyStep);
   unsigned long t = millis();
-  if (strategyState == 'A') { // braking
-    if ((t - strategyStateStart) < TIME_TO_BRAKE) { // let time to brake
+  if (strategyStep == 'A') { // braking
+    if ((t - strategyStepStart) < TIME_TO_BRAKE) { // let time to brake
       //bluetooth.println("let time to brake ");
       return;
     }
-    startStrategyState('B');
+    startstrategyStep('B');
     reverse();
-    accelerate(50);
-    int newdir = random(0, 2);
-    if (newdir == 1) {
+    accelerate(60);
+    
+    if ((millis() - last_brake_and_back_time) > TIME_UNIQUE_MANEUVER){
+      brake_and_back_direction = random(0, 2);
+    }
+    if (brake_and_back_direction == 1) {
       steer(MAX_STEER);
     } else {
       steer(MAX_STEER * -1);
     }
   }
-  if (strategyState == 'B') { // steering back
-    if ((t - strategyStateStart) < 2000) { // let time to steer back
+  if (strategyStep == 'B') { // steering back
+    if ((t - strategyStepStart) < 2000) { // let time to steer back
       //bluetooth.println("let time to steer back");
       return;
     }
-    startStrategyState('C');
+    startstrategyStep('C');
     accelerate(-1);
   }
-  if (strategyState == 'C') { // steering forward
-    if ((t - strategyStateStart) < TIME_TO_BRAKE) { // let time to brake
+  if (strategyStep == 'C') { // steering forward
+    if ((t - strategyStepStart) < (TIME_TO_BRAKE/2)) { // let time to brake
       //bluetooth.println("let time to brake ");
       return;
     }
-    startStrategyState('D');
+    startstrategyStep('D');
     forward();
     steer(cur_steering * -1);
-    accelerate(50);
+    accelerate(60);
   }
 
-  if (strategyState == 'D') { // steering forward
-    if (t - strategyStateStart > 2000) { // let time to steer forward
+  if (strategyStep == 'D') { // steering forward
+    if (last_dist < (DIST_BRAKE/2)){ // the reverse run is not enought, some more back
+      startstrategyStep('A');
+      return;
+    }
+    if (t - strategyStepStart > 2000) { // let time to steer forward
       strategyLock = false; // release the lock and auto define a new strategy
-      bluetooth.println("strategyLock = false");
+      btLog = btLog + "# strategyLock = false\n";
       goAhead();
+      last_brake_and_back_time = millis();
     }
   }
 
@@ -346,7 +445,15 @@ void steerHalf() {
   bool start = startStrategy('H');
   if (start) {
     accelerate(100);
-    int newdir = random(0, 2);
+    int newdir;
+    if (cur_steering==0){ // going straight
+      newdir = random(0, 2); // choose a random direction
+    } else if(cur_steering > 0){ // yet steering (full)
+      newdir == 1;               // steer half in the same direction 
+    } else {
+      newdir == 0;
+    }
+    
     if (newdir == 1) {
       steer(MAX_STEER / 2);
     } else {
@@ -359,8 +466,16 @@ void steerHalf() {
 void steerFull() {
   bool start = startStrategy('F');
   if (start) {
+    int newdir;
     accelerate(75);
-    int newdir = random(0, 2);
+    if (cur_steering==0){ // going straight
+      newdir = random(0, 2); // choose a random direction
+    } else if(cur_steering > 0){ // yet steering (half)
+      newdir == 1;               // steer full in the same direction 
+    } else {
+      newdir == 0;
+    }
+    
     if (newdir == 1) {
       steer(MAX_STEER);
     } else {
@@ -373,7 +488,9 @@ void steer(int val) { // steer angle from -90 to +90
   if (cur_steering == val) {
     return;
   }
-  steering_servo.write(val + 90 + STEER_TRIM); // servo need angle from 0 to 180
+  if (DEBUG_NO_MOTOR == false) {
+    steering_servo.write(val + 90 + STEER_TRIM); // servo need angle from 0 to 180
+  }
   cur_steering = val;
   //bluetooth.print("Steering: ");
   //bluetooth.println(val);
@@ -405,7 +522,9 @@ void motorLoop() {
   }
 
   if (target_speed_A < speed_A) { // decelerate
-    analogWrite(A_motor_pwm, map(target_speed_A, 0, 100, 0, 255));
+    if (DEBUG_NO_MOTOR == false) {
+      analogWrite(A_motor_pwm, map(target_speed_A, 0, 100, 0, 255));
+    }
     speed_A = target_speed_A;
   }
 
@@ -416,7 +535,9 @@ void motorLoop() {
       if (speed_A < MIN_SPEED) {
         speed_A = MIN_SPEED;
       }
-      analogWrite(A_motor_pwm, map(speed_A, 0, 100, 0, 255));
+      if (DEBUG_NO_MOTOR == false) {
+        analogWrite(A_motor_pwm, map(speed_A, 0, 100, 0, 255));
+      }
       last_speed_change = millis();
     }
   }
@@ -428,85 +549,71 @@ void motorLoop() {
 
 }
 
-long testDistance() {
-  //porta bassa l'uscita del trigger
-  digitalWrite(Dist_trig, LOW);
+//long testDistance() {
+//  //porta bassa l'uscita del trigger
+//  digitalWrite(Dist_trig, LOW);
+//
+//  //invia un impulso di 10microsec su trigger
+//  digitalWrite(Dist_trig, HIGH);
+//  delayMicroseconds( 10 );
+//  digitalWrite(Dist_trig, LOW);
+//  long r;
+//  long duration = pulseIn(Dist_echo, HIGH, 38001);
+//  delay(10);
+//  r = 0.034 * duration / 2;
+//  return r;
+//}
 
-  //invia un impulso di 10microsec su trigger
-  digitalWrite(Dist_trig, HIGH);
-  delayMicroseconds( 10 );
-  digitalWrite(Dist_trig, LOW);
-  long r;
-  long duration = pulseIn(Dist_echo, HIGH, 38001);
-  delay(10);
-  r = 0.034 * duration / 2;
-  return r;
-}
-
-long checkDistance() {
-  long r1 = testDistance();
-  long r2 = testDistance();
-  long r3 = testDistance();
-  long m = (r1 + r2 + r3) / 3;
-  if (abs(r1 - m) + abs(r2 - m) + abs(r3 - m) > 60) {
-    bluetooth.print("anomalia distanza - media: ");
-    bluetooth.print(m);
-    bluetooth.print(" - ");
-    bluetooth.print(r1);
-    bluetooth.print(", ");
-    bluetooth.print(r2);
-    bluetooth.print(", ");
-    bluetooth.println(r3);
-  }
-  return m;
-}
+//long checkDistance() {
+//  long r1 = testDistance();
+//  long r2 = testDistance();
+//  long r3 = testDistance();
+//  long m = (r1 + r2 + r3) / 3;
+//  if (abs(r1 - m) + abs(r2 - m) + abs(r3 - m) > 60) {
+//    bluetooth.print("anomalia distanza - media: ");
+//    bluetooth.print(m);
+//    bluetooth.print(" - ");
+//    bluetooth.print(r1);
+//    bluetooth.print(", ");
+//    bluetooth.print(r2);
+//    bluetooth.print(", ");
+//    bluetooth.println(r3);
+//  }
+//  return m;
+//}
 
 long checkDistanceNew() {
+  //unsigned long timer = millis();
   return sonar.convert_cm(sonar.ping_median(5));
+  //Serial.print("leggo sonar: ");
+  //Serial.println(millis() - timer);
+  //return d;
 }
 
+//void simpLinReg(float* x, float* y, float* lrCoef, int n){
+//  // pass x and y arrays (pointers), lrCoef pointer, and n.  The lrCoef array is comprised of the slope=lrCoef[0] and intercept=lrCoef[1].  n is length of the x and y arrays.
+//  // http://en.wikipedia.org/wiki/Simple_linear_regression
+//  // http://jwbrooks.blogspot.it/2014/02/arduino-linear-regression-function.html
+//  // initialize variables
+//  float xbar=0;
+//  float ybar=0;
+//  float xybar=0;
+//  float xsqbar=0;
+//  
+//  // calculations required for linear regression
+//  for (int i=0; i<n; i++){
+//    xbar=xbar+x[i];
+//    ybar=ybar+y[i];
+//    xybar=xybar+x[i]*y[i];
+//    xsqbar=xsqbar+x[i]*x[i];
+//  }
+//  xbar=xbar/n;
+//  ybar=ybar/n;
+//  xybar=xybar/n;
+//  xsqbar=xsqbar/n;
+//  
+//  // simple linear regression algorithm
+//  lrCoef[0]=(xybar-xbar*ybar)/(xsqbar-xbar*xbar);
+//  lrCoef[1]=ybar-lrCoef[0]*xbar;
+//}
 
-void test() {
-  int direzione;
-  // put your main code here, to run repeatedly:
-  int brakebtn = digitalRead(7);
-  if (brakebtn != last_brake) {
-    last_brake = brakebtn;
-    if (brakebtn) {
-      analogWrite(3, 0);
-      digitalWrite(9, 1);
-      bluetooth.println("freno");
-      return;
-    } else {
-      digitalWrite(9, 0);
-    }
-  }
-
-
-  int dirbtn = digitalRead(6);
-  //if (last_dirbtn == 0 && dirbtn == 1){
-  //  last_dirbtn = dirbtn;
-  direzione = !direzione;
-  digitalWrite(12, direzione);
-  bluetooth.print("direzione: ");
-  bluetooth.println(direzione);
-  //}
-  //last_dirbtn = dirbtn;
-
-  int btn_speed_A = analogRead(A2);
-  if (btn_speed_A != speed_A) {
-    digitalWrite(12, direzione);
-    analogWrite(3, map(0, 255, 0, 1023, btn_speed_A));
-    bluetooth.print("velocita: ");
-    bluetooth.print(direzione);
-    bluetooth.println(btn_speed_A);
-    speed_A = btn_speed_A;
-  }
-  int last_consumo;
-  int consumo = analogRead(A0);
-  if (consumo != last_consumo) {
-    bluetooth.print("consumo: ");
-    bluetooth.println(consumo);
-    last_consumo = consumo;
-  }
-}
--- autopilot1/main.py
+++ autopilot1/main.py
@@ -8,20 +8,72 @@ from collections import deque
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.label import Label
-from kivy.properties import NumericProperty, ReferenceListProperty,\
-    ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, StringProperty
 from kivy.clock import Clock
 from kivy.graphics import Color,Line
-
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.event import EventDispatcher
+#from kivy.garden import gauge
 
 import serial
 from serial.tools.list_ports import comports
+
+from kivy.core.window import Window
+#Window.clearcolor = (1, 1, 1, 1)
 
 __version__ = '0.0.1'
 
 TARGET_ARDUINO = '55431313338351113152'
 MAX_SENSOR_DISTANCE = 200
 
+
+
+class ArduinoData(EventDispatcher):
+    distance = NumericProperty(0)
+    cur_steering = NumericProperty(0)
+    dir_A = NumericProperty(1)
+    brake_A = NumericProperty(0)
+    speed_A = NumericProperty(0)
+    target_speed_A = NumericProperty(70)
+    current = NumericProperty(0)
+    strategyLock = StringProperty('-')
+    strategy = StringProperty('G')
+    strategyTime = NumericProperty(0)
+    strategyStep = StringProperty('-')
+    strategyStepTime = NumericProperty(0)
+    lastCommCicles = NumericProperty(0)
+    lastCommTime = NumericProperty(0)
+    auto = StringProperty('normal')
+    
+    @property
+    def auto_mode(self):
+        return self.auto == 'down'
+    
+    def readArduinoData(self, datastring):
+        d = datastring.split(';')
+        millis = int(d[0])
+        self.distance = int(d[1])
+        self.cur_steering = int(d[2])
+        self.dir_A = int(d[3])
+        self.brake_A = int(d[4])
+        self.speed_A = int(d[5])
+        self.target_speed_A = int(d[6])
+        self.current = float(d[7])
+        self.strategyLock = 'y' if int(d[8]) else '-'
+        self.strategy = d[9]
+        strategyStart = int(d[10])
+        self.strategyTime = millis - strategyStart
+        self.strategyStep = d[11]
+        strategyStepStart = int(d[12])
+        self.strategyStepTime = millis - strategyStepStart
+        self.lastCommCicles = int(d[13])
+        lastCommTime = int(d[14])
+        self.lastCommTime = millis - lastCommTime
+    
+    
 class RadarPulse(Widget):
     def __init__(self, parent, angle, distance, *args, **kwargs):
         self.angle = angle
@@ -32,47 +84,19 @@ class RadarPulse(Widget):
         print self.angle
         print self.distance
         print self.center
-        
-class RadarGrid(Widget):
+
+class BottomCommands(BoxLayout):
     pass
-    
-class RadarPage(Widget):
+
+        
+class RadarGrid(FloatLayout):
     lbl_angle = ObjectProperty(None)
     lbl_dist = ObjectProperty(None)
-    radar_grid = ObjectProperty(None)
-    lbl_port = ObjectProperty(None)
-    lbl_log = ObjectProperty(None)
     
     def __init__(self, *args, **kwargs):
         self.targets = {}
-        super(RadarPage, self).__init__(*args, **kwargs)
-        self.port = None
-        self.serial = None
-        self.lastConnectionAttempt = 0
-        self.loglines = deque(maxlen=5)
-        self.connectArduino()
-        self.lastangle = None
-        
-    def on_touch_down(self, touch):
-        #if self.lastangle:
-        #    print self.targets.items()
-        #    self.remove_widget(self.targets.pop(0))
-        #    print self.targets.items()
-        #    self.lastangle = False
-        #else:
-        #    angle = 90
-        #    distance = 100
-        #    self.lastangle = True
-        #    self.updateTarget(angle, distance)
-        
-        newstep = int(random() * 30)
-        self.log(newstep)
-        self.writeArduino(newstep)
-        for angle in self.targets.keys():
-            self.remove_widget(self.targets.pop(angle))
-            self.log('cancello angolo: %i' % angle)
-        return True
-        
+        super(RadarGrid, self).__init__(*args, **kwargs)
+
     def updateTarget(self, angle, dist):
         if dist < 0: return
         if dist > MAX_SENSOR_DISTANCE: return
@@ -81,28 +105,80 @@ class RadarPage(Widget):
         print "dist: %.2f" % dist
 
         if self.targets.get(angle): self.remove_widget(self.targets.pop(angle))
-        self.targets[angle] = RadarPulse(self.radar_grid, angle, dist)
+        self.targets[angle] = RadarPulse(self, angle, dist)
         self.add_widget(self.targets[angle])
         
         self.lbl_angle.text = '%.2f' % angle
         self.lbl_dist.text = '%.2f' % dist
         
+#    def on_touch_down_(self, touch):
+#        #if self.lastangle:
+#        #    print self.targets.items()
+#        #    self.remove_widget(self.targets.pop(0))
+#        #    print self.targets.items()
+#        #    self.lastangle = False
+#        #else:
+#        #    angle = 90
+#        #    distance = 100
+#        #    self.lastangle = True
+#        #    self.updateTarget(angle, distance)
+#        
+#        newstep = int(random() * 30)
+#        self.log(newstep)
+#        self.writeArduino(newstep)
+#        for angle in self.targets.keys():
+#            self.remove_widget(self.targets.pop(angle))
+#            self.log('cancello angolo: %i' % angle)
+#        return True
+        
+class LeftCommands(BoxLayout):
+    lbl_port = ObjectProperty(None)
+    lbl_log = ObjectProperty(None)
+
+class AutopilotPage(BoxLayout):
+    radarGrid = ObjectProperty(None)
+    leftCommands = ObjectProperty(None)
+
+class AutopilotApp(App):
+    data = ObjectProperty(ArduinoData(), rebind=True)
+    
+    def __init__(self, *args, **kwargs):
+        super(AutopilotApp, self).__init__(*args, **kwargs)
+        self.port = None
+        self.serial = None
+        self.lastConnectionAttempt = 0
+        self.loglines = deque(maxlen=5)
+        self.segno = 1
+        
+    def build(self):
+        radar = AutopilotPage()
+        Clock.schedule_interval(self.update, 1.0 / 100.0)
+        self.connectArduino()
+        return radar
+        
+    
     def connectArduino(self):
         if time.time() - self.lastConnectionAttempt < 1: return self.port
         self.lastConnectionAttempt = time.time()
         self.log('provo a connettere Arduino')
-        port = [p for p in comports() if TARGET_ARDUINO in p[2]]
+        #port = [p for p in comports() if TARGET_ARDUINO in p[2]]
+        port = [p for p in comports() if 'HC-06' in p[0]]
         # get the right port based on the SN of target arduino
         if port:
             self.port = port[0][0] 
-            self.serial = serial.Serial(self.port, 250000, timeout=1)
-            self.log('Arduino connesso su porta %s' % self.port)
+            try:
+                self.serial = serial.Serial(self.port, 115200, timeout=1)
+                self.log('Arduino connesso su porta %s' % self.port)
+            except:
+                self.log('Errore di connessione su porta %s' % self.port)
+                pass
+            
         else:
             self.port = None
             self.serial = None
             self.log('non trovato Arduino')
             
-        self.lbl_port.text = str(self.port or 'no connection to Arduino')
+        #self.lbl_port.text = str(self.port or 'no connection to Arduino')
         return self.port
         
     def readArduino(self):
@@ -116,28 +192,37 @@ class RadarPage(Widget):
             self.log(line)
             return
         print line
-        angle, dist = [int(v) for v in line.split(';')]
-        self.updateTarget(angle, dist)
+        if True:
+            self.data.readArduinoData(line)
+        #except:
+        #    self.log('** transmission error **')
+        #angle, dist = [int(v) for v in line.split(';')]
+        #self.radarGrid.updateTarget(angle, dist)
 
-    def writeArduino(self, newstep):
+    def writeArduino(self):
         if not self.serial: return
-        self.serial.write('%i\n' % (newstep, ))
+        if self.data.auto_mode:
+            self.serial.write('A')
+        else:
+            self.serial.write('M%i%20i%20i%i\n' % (self.data.dir_A, 
+            		self.data.speed_A, self.data.cur_steering, self.data.brake_A))
+
 
     def update(self, dt):
         self.readArduino()
+        self.writeArduino()
+        
+        #if self.data.target_speed_A == 100:
+        #    self.segno = -1
+        #elif self.data.target_speed_A == 0:
+        #    self.segno = 1
+        #self.data.target_speed_A += (1 * self.segno)
+        #self.data.cur_steering += (1 * self.segno)
         
     def log(self, txt):
         print txt
         self.loglines.append(str(txt))
-        self.lbl_log.text = '\n'.join(self.loglines)
-
-class RadarApp(App):
-    def build(self):
-        radar = RadarPage()
-        Clock.schedule_interval(radar.update, 1.0 / 100.0)
-        return radar
-        
-
+        #self.leftCommands.lbl_log.text = '\n'.join(self.loglines)
 
 if __name__ == '__main__':
-    RadarApp().run()
+    AutopilotApp().run()
